dc03ad7b7d33339b90fad8d90f3c87a8fb5db853
VictorB <victorb62@byui.edu>
2023-01-09 22:45:52 -0700

    add demo_app

--- w01/server.js
+++ w01/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+// const express = require('express');
 // const bodyParser = require('body-parser');
 // const mongodb = require('./bd/connect');
 
-const app = express();
+// const app = express();
 const port = process.env.PORT || 80;
 
-app.use('/', require('./routes'))
+// app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
