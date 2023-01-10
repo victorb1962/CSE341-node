5653e14e9003b5b4a364cce462fc184c918d5b01
VictorB <victorb62@byui.edu>
2023-01-09 23:30:47 -0700

    week1

--- w01/controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayJoke = (req, res) => {
-  const data =
-    'How did the telephone propose to his girlfriend? ...he gave her a ring.';
-  res.status(200).send(data);
-};
-
-module.exports = {
-  displayJoke,
-};
--- w01/routes/index.js
+++
@@ -1,6 +0,0 @@
-const routes = require('express').Router();
-const jokes = require('../controllers/');
-
-routes.get('/', jokes.displayJoke);
-
-module.exports = routes;
